326f7095b85e20a078592c33b635c0ab16503dc8
Steve Godin <steve.god@gmail.com>
2018-06-18 10:58:10 -0700

    Add knexfile instruction for linux users

--- js_express_with_postgres/express_demo/knexfile.js
+++ js_express_with_postgres/express_demo/knexfile.js
@@ -5,6 +5,12 @@ module.exports = {
     client: "pg",
     connection: {
       database: "express_demo"
+      // For linux users, you must set a username and password.
+      // To change your password, use psql:
+      // $ psql
+      // sg=# ALTER USER username PASSWORD 'supersecret';
+      // username: "sg",
+      // password: "supersecret"
     },
     migrations: {
       tableName: "knex_migrations",
